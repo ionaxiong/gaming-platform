4cde7944595c22f7c939875c8aa1382419aa9461
ming xiong <bgm182@myy.haaga-helia.fi>
2021-04-27 19:03:00 +0300

    add RoleRepository

---
+++ src/main/java/com/example/Gamesite/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package com.example.Gamesite.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import com.example.Gamesite.model.Role;
+
+public interface RoleRepository extends CrudRepository<Role, Long> {
+
+}
--- src/main/java/com/example/Gamesite/repository/UserRepository.java
+++ src/main/java/com/example/Gamesite/repository/UserRepository.java
@@ -1,19 +1,11 @@
 package com.example.Gamesite.repository;
 
 import java.util.List;
-
 import org.springframework.data.repository.CrudRepository;
-
 import com.example.Gamesite.model.User;
 
 public interface UserRepository extends CrudRepository<User, Long>{
-	//find user with the given identifier
 	User findByUserId(Long userId);
 	User findByUsername(String username);
 	User findByEmail(String email);
-//	List<User> findAll();
-//	
-//	//delete user with the given identifier
-//	void deleteByUserId(String userId);
-//	void deleteByUsername(String username);
 }
