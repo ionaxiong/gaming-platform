e52491789693d1f35cad54bf5761b97ab22e2c7a
ming xiong <bgm182@myy.haaga-helia.fi>
2021-04-27 21:39:22 +0300

    add logout function to gamelist.html

--- src/main/java/com/example/Gamesite/web/GameSiteController.java
+++ src/main/java/com/example/Gamesite/web/GameSiteController.java
@@ -19,60 +19,61 @@ import com.example.Gamesite.service.UserService;
 public class GameSiteController {
 	@Autowired
 	private GameRepository grepository;
-	
+
 	@Autowired
 	private UserGameScoreRepository srepository;
-	
+
 	@Autowired
 	private UserService userService;
-	
+
 	@Autowired
 	private SecurityService securityService;
-	
+
 	@Autowired
 	private UserValidator userValidator;
-	
+
 	@GetMapping("/registration")
 	public String registration(Model model) {
 		if (securityService.isAuthenticated()) {
 			return "redirect:/";
 		}
+
 		model.addAttribute("userForm", new User());
-		
+
 		return "registration";
 	}
-	
+
 	@PostMapping("/registration")
 	public String registration(@ModelAttribute("userForm") User userForm, BindingResult bindingResult) {
 		userValidator.validate(userForm, bindingResult);
-		
+
 		if (bindingResult.hasErrors()) {
 			return "registration";
 		}
-		
+
 		userService.save(userForm);
-		
+
 		securityService.autoLogin(userForm.getUsername(), userForm.getPasswordConfirm());
-		
+
 		return "redirect:/gamelist";
 	}
-	
+
 	@GetMapping("/login")
 	public String login(Model model, String error, String logout) {
 		if (securityService.isAuthenticated()) {
-			return "redirect:/gamelist";
+			return "redirect:/";
 		}
-		
+
 		if (error != null)
-			model.addAttribute("error","Your username and password is invalid!");
-		
+			model.addAttribute("error", "Your username and password is invalid!");
+
 		if (logout != null)
-			model.addAttribute("message","You have been logged out successfully!");
-		
+			model.addAttribute("message", "You have been logged out successfully!");
+
 		return "login";
 	}
-	
-	@RequestMapping(value= {"/", "/games"})
+
+	@RequestMapping(value = { "/", "/gamelist" })
 	public String games(Model model) {
 		model.addAttribute("games", grepository.findAll());
 		return "gamelist";
